b733412e54541055145089d2a841ad9bd1606f0b
Funhuninja Studio <funhuninja@gmail.com>
2022-07-14 20:40:22 +0300

    Added Project Wizard

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Appazoid-CLI
 CLI for Appazoid
+
+# Requirements:
+
+Python
+
+VCPKG (optional)
+
+# How To Setup:
+run `setup.py` to run the validation and install the cli.
+
+# How To Use:
+open `cli/` folder and run `az_cli.py --help` to see all the commands.
+
+To create a project run `az_cli.py new-project <<project name>> <<path>>` 
+
+To create a project using the project wizard run `az_cli.py project-wizard`
+
+![image](https://user-images.githubusercontent.com/51212450/179031070-0f6e9d66-9a24-4372-b7c2-97cdd5a777ab.png)
--- az_cli.py
+++ az_cli.py
@@ -1,8 +1,12 @@
+import json
+from pprint import pprint
 import sys
 import os
+from wsgiref import validate
 import typer
 import pyfiglet
-from datetime import datetime
+from datetime import date
+import inquirer
 
 ########  PRINTING  ###################
 def __prt_clr_help(str,color):
@@ -31,28 +35,129 @@ def color_print(str, color):
 
 az_title_art = pyfiglet.figlet_format("APPAZOID      CLI")
 az_project_wizard_art = pyfiglet.figlet_format("PROJ    WIZARD")
+az_project_wizart_ascii = """
+                             /\\
+                            /  \\
+                           |    |
+                         --:'''':--
+                           :'_' :
+                           _:"":\___
+            ' '      ____.' :::     '._
+           . *=====<<=)           \    :
+            .  '      '-'-'\_      /'._.'
+                             \====:_ ""
+                            .'     \\
+                           :       :
+                          /   :    \\
+                         :   .      '.
+         ,. _        snd :  : :      :
+      '-'    ).          :__:-:__.;--'
+    (        '  )        '-'   '-'
+ ( -   .00.   - _
+(    .'  _ )     )
+'-  ()_.\,\,   -
+"""
 #should_close = False
 app = typer.Typer()
 
+def change_project_json(path:str,project_name:str, render_api = "AZ_RENDER_API_OPENGL",cmake_version = "3.6"):
+    json_path = os.path.join(path,"project.json")
+    json_path = json_path.replace("\\\\","/")
+    json_path = json_path.replace("\\","/")
+    json_file = open(json_path,"r")
+    data = json.load(json_file)
+    data["project"]["name"] = project_name
+    data["project"]["render_api"] = render_api
+    data["cmake_minimum_version"] = cmake_version
+    json_formatted_str = json.dumps(data, indent=2)
+    json_file.close()
+    json_file = open(json_path,"w")
+    json_file.write(json_formatted_str)
+    return
+
+def clone_repo(project_name:str, project_path:str,vcpkg:bool = False):
+    project_whole_path = os.path.join(project_path,project_name)
+    if(os.path.isdir(project_whole_path)):
+        if(os.listdir(project_whole_path) != 0):
+            color_print("WARNING: directory is not empty.","yellow")
+            return False
+    color_print(f"Creating '{project_name}' at '{project_path}'","green")
+    if(vcpkg):
+        os.system(f"git clone --recursive https://github.com/Byte-White/Appazoid-Project-Template-VCPKG \"{project_whole_path}\"")        
+    else:
+        os.system(f"git clone --recursive https://github.com/Byte-White/Appazoid-Project-Template \"{project_whole_path}\"")
+    return True
+
 @app.command()
 def new_project(project_name:str, project_path:str):
-    if(os.path.isdir(project_path+'\\'+project_name)):
-        if(os.listdir(project_path+'\\'+project_name) != 0):
-            color_print("WARNING: directory is not empty.","yellow")
-            return
-    color_print(f"Creating '{project_name}' at '{project_path}'","green")
-    os.system(f"git clone --recursive https://github.com/Byte-White/Appazoid-Project-Template {project_path}/{project_name}")
+    project_whole_path = os.path.join(project_path,project_name)
+    if(clone_repo(project_name,project_path)):
+        change_project_json(project_whole_path,project_name)
         
 
 #@app.command()
 #def exit():
 #    global should_close
 #    should_close = True
+todays_date = date.today()
+
+def validate_project_name(answers, proj_name):
+    if(proj_name == ""):
+        print("Can't think of a name? Well think harder.")
+        return False
+    proj_path = os.path.join(answers["path"],proj_name)
+    proj_path = proj_path.replace("\\\\","/")
+    proj_path = proj_path.replace("\\","/")
+    if(os.path.isdir(proj_path)):
+        if(os.listdir(proj_path).__len__() != 0):
+            color_print("Warning: Directory is not empty! Please change the name of the project.")
+            return False
+    return True
+
+questions = [
+    inquirer.Path(
+        "path",
+        message = "select a path ",
+        
+    ),
+
+    inquirer.Text(
+        "name",
+        message = "what is your project's name?",  
+        validate= validate_project_name
+    ),
+
+    inquirer.Text(
+        
+        "cmake_version",
+        message = "what is the minimum cmake version?",
+        default="3.6"
+    ),
+    
+    inquirer.List(
+        
+        "render_api",
+        message = "what rendering api do you want to use?",
+        choices=["OpenGL","Vulkan"],
+        default="OpenGL"
+    ),
+    inquirer.Checkbox(
+        "vcpkg",
+        message= "would you like to use vcpkg template? (X=True ; o=False)",
+        choices=["vcpkg"],
+    ),
+]
 
 @app.command()
 def project_wizard():
     print(az_project_wizard_art)
-    print("Todo: add inquirer")
+    print(az_project_wizart_ascii)
+    global questions
+    answers = inquirer.prompt(questions)
+    if(clone_repo(answers["name"],answers["path"])):
+        change_project_json(os.path.join(answers["path"],answers["name"]),answers["name"],answers["render_api"],answers["cmake_version"])
+    
+    
 
 
 if(__name__ == "__main__"):
